b7698db1b78c60456486e3717a6609c36fec1e87
Tony Vo <ttrung.vo@gmail.com>
2026-03-15 09:23:35 -0600

    island perimeter

--- python/src/binarysearch/countcompletetreenodes.py
+++ python/src/binarysearch/countcompletetreenodes.py
@@ -7,30 +7,5 @@ class CountCompleteTreeNodes:
     inclusive at the last level h.
 
     Design an algorithm that runs in less than O(n) time complexity.
+    todo: next week, reason/justify the solutions and why it's log(n) and disable code completion for the session
     """
-
-    def get_left_depth(self, node):
-        depth = 0
-        while node:
-            depth += 1
-            node = node.left
-        return depth
-
-    def get_right_depth(self, node):
-        depth = 0
-        while node:
-            depth += 1
-            node = node.right
-        return depth
-
-    def countNodes(self, root):
-        if not root:
-            return 0
-
-        left_depth = self.get_left_depth(root)
-        right_depth = self.get_right_depth(root)
-
-        if left_depth == right_depth:
-            return (2 ** left_depth) - 1
-        else:
-            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
---
+++ python/src/islandperimeter.py
@@ -0,0 +1,10 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        return 0
+    #     // go through all of cells
+    #     //    and return the cells that is connect
+    #     //    and return thet neighbors for each cell
+    #     [totalLandCells, totalNeighborsForLandCells] = filterConnectedLand()
+    #     return totalLandCells*4 - totalNeighborsForLandCells
+    #
+    # def filterConnectedLand():
